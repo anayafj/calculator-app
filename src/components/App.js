207f71e6b33a2bec77883854ad93833e4155f4d1
anayafj <franciscojanaya80@gmail.com>
2021-02-16 17:05:05 -0800

    got numbers displaying. decimal and +\- works too

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,60 @@ import React, { Component } from 'react';
 import NumberDisplay from './NumberDisplay';
 import NumberPad from './NumberPad';
 
+const padNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const padOperators = ['AC', '+/-', '%', '÷', 'x', '-', '+', '.', '='];
+
 class App extends Component {
 	state = {
 		numbers: [],
 		opertaters: [],
-		number: 0,
+		number: '0',
+	};
+
+	buttonSelected = (selection) => {
+		console.log('Button selected = ', selection);
+		// console.log(Number(selection));
+		const currentNum = this.state.number;
+
+		if (!isNaN(Number(selection))) {
+			console.log('Yes it is a number');
+
+			// check if first number is 0. If so replace.
+			if (Number(currentNum) == 0 && currentNum.length <= 1) {
+				this.setState({ number: selection });
+			} else {
+				// if number then add to end of state number
+				this.setState({ number: currentNum.concat(selection) });
+			}
+		} else {
+			// check if operator is special
+			switch (padOperators.indexOf(selection)) {
+				case 0:
+					console.log('CLEAR');
+					break;
+				case 1:
+					console.log('Negative/positive');
+					this.setState({ number: String(Number(currentNum) * -1) });
+					break;
+				case 2:
+					console.log('%');
+					break;
+				case 7:
+					console.log('.');
+					// check if there is already a decimal. if so ignore
+					if (currentNum.indexOf('.') === -1) {
+						console.log('No decimal found. Add it');
+						this.setState({ number: currentNum.concat(selection) });
+					}
+					break;
+				case 8:
+					console.log('Calculate');
+					break;
+				default:
+					console.log('Default');
+			}
+			console.log('Not a number');
+		}
 	};
 
 	onButtonPressed = (selection) => {
@@ -19,11 +68,19 @@ class App extends Component {
 		console.log('Equation Solved', num);
 	};
 
+	componentDidUpdate() {
+		console.log('Concat number = ', this.state.number);
+	}
+
 	render() {
 		return (
 			<div className="app-container">
 				<NumberDisplay displayNumber={this.state.number} />
-				<NumberPad />
+				<NumberPad
+					padNum={padNumbers}
+					padOper={padOperators}
+					buttonSelected={this.buttonSelected}
+				/>
 			</div>
 		);
 	}
--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -4,101 +4,45 @@ import React, { Component } from 'react';
 import NumberButton from './NumberButton';
 
 class NumberPad extends Component {
+	// state = { num : null, operator: ''}
+
 	onButtonPress = (e) => {
-		console.log('Button Pressed', e);
+		this.props.buttonSelected(e);
 	};
 
 	render() {
-		const padNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-		const padOperators = ['AC', '+/-', '%', '÷', 'x', '-', '+', '.', '='];
+		const { buttonSelected, padNum, padOper } = this.props;
 
 		return (
 			<div className="number-pad">
 				<div className="row">
-					<NumberButton
-						name={padOperators[0]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[1]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[2]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[3]}
-						onButtonPress={this.onButtonPress}
-					/>
+					<NumberButton name={padOper[0]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[1]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[2]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[3]} onButtonPress={this.onButtonPress} />
 				</div>
 				<div className="row">
-					<NumberButton
-						name={padNumbers[7]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[8]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[9]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[4]}
-						onButtonPress={this.onButtonPress}
-					/>
+					<NumberButton name={padNum[7]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[8]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[9]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[4]} onButtonPress={this.onButtonPress} />
 				</div>
 				<div className="row">
-					<NumberButton
-						name={padNumbers[4]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[5]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[6]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[5]}
-						onButtonPress={this.onButtonPress}
-					/>
+					<NumberButton name={padNum[4]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[5]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[6]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[5]} onButtonPress={this.onButtonPress} />
 				</div>
 				<div className="row">
-					<NumberButton
-						name={padNumbers[1]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[2]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padNumbers[3]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[6]}
-						onButtonPress={this.onButtonPress}
-					/>
+					<NumberButton name={padNum[1]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[2]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padNum[3]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[6]} onButtonPress={this.onButtonPress} />
 				</div>
 				<div className="row">
-					<NumberButton
-						name={padNumbers[0]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[7]}
-						onButtonPress={this.onButtonPress}
-					/>
-					<NumberButton
-						name={padOperators[8]}
-						onButtonPress={this.onButtonPress}
-					/>
+					<NumberButton name={padNum[0]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[7]} onButtonPress={this.onButtonPress} />
+					<NumberButton name={padOper[8]} onButtonPress={this.onButtonPress} />
 				</div>
 			</div>
 		);
