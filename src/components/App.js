5c4640f70b791f4370d6bded90133240ebcb65da
anayafj <franciscojanaya80@gmail.com>
2021-02-18 10:58:18 -0800

    need to work on updating state array

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,19 @@ import NumberPad from './NumberPad';
 
 const padNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const padOperators = ['AC', '+/-', '%', '÷', 'x', '-', '+', '.', '='];
+let operatorSelected = false;
 
 class App extends Component {
 	state = {
 		numbers: [],
-		opertaters: [],
+		operators: [],
 		number: '0',
 	};
 
 	buttonSelected = (selection) => {
-		console.log('Button selected = ', selection);
-		// console.log(Number(selection));
+		// console.log('Button selected = ', selection);
+		console.log('operatorsSelected = ', operatorSelected);
+
 		const currentNum = this.state.number;
 
 		if (!isNaN(Number(selection))) {
@@ -24,19 +26,51 @@ class App extends Component {
 
 			// check if first number is 0. If so replace.
 			if (Number(currentNum) == 0 && currentNum.length <= 1) {
+				if (operatorSelected) operatorSelected = false;
 				this.setState({ number: selection });
 			} else {
-				// if number then add to end of state number
-				this.setState({ number: currentNum.concat(selection) });
+				// look to see if new number needs to start
+				if (operatorSelected) {
+					// new number. just set
+					this.setState({ number: selection });
+					operatorSelected = false;
+				} else {
+					// if number then add to end of state number
+					this.setState({ number: currentNum.concat(selection) });
+				}
 			}
 		} else {
+			const operatorIndex = padOperators.indexOf(selection);
+			if (operatorSelected) {
+				if (operatorIndex == 0 || operatorIndex == 7) {
+					// check if operator is special
+					switch (operatorIndex) {
+						case 0:
+							// AC
+							this.setState({
+								numbers: [],
+								operators: [],
+								number: '0',
+							});
+							break;
+						case 7:
+							// .
+							if (currentNum.indexOf('.') === -1)
+								this.setState({ number: currentNum.concat(selection) });
+							break;
+					}
+				}
+				operatorSelected = false;
+				return;
+			}
+
 			// check if operator is special
 			switch (padOperators.indexOf(selection)) {
 				case 0:
 					// AC
 					this.setState({
 						numbers: [],
-						opertaters: [],
+						operators: [],
 						number: '0',
 					});
 					break;
@@ -59,6 +93,12 @@ class App extends Component {
 					break;
 				default:
 					console.log('Default');
+					// all other operators: ÷, x, -, +
+					operatorSelected = true;
+					this.setState({
+						numbers: [].push(currentNum),
+						operators: [].push(selection),
+					});
 			}
 		}
 	};
@@ -73,6 +113,7 @@ class App extends Component {
 
 	componentDidUpdate() {
 		console.log('State of number = ', this.state.number);
+		console.log('State - ', this.state);
 	}
 
 	render() {
